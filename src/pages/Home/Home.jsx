72934898a25452a8f57e0879fd1c94bd3f621581
Rayka Irawan <raykairawan123@gmail.com>
2023-07-11 15:44:12 +0700

    add home page & project implemented

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,24 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import moment from 'moment';
+
 import useProject from '../../store/useProject';
+import useAuthStore from '../../store/useAuthStore';
+
+import './Home.scss';
 
 const Home = () => {
+  const token = useAuthStore((state) => state.token);
   const { projects, setProjects } = useProject();
 
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:3000//projects.json');
+        const response = await axios.get('http://127.0.0.1:3000/projects.json', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
         const projectsData = response.data.projects;
         setProjects(projectsData);
       } catch (error) {
@@ -17,19 +27,34 @@ const Home = () => {
     };
 
     fetchProjects();
-  }, [setProjects]);
+  }, [token, setProjects]);
 
   return (
     <div>
       <h1>Selamat Datang di Redmine</h1>
-      <h1>Daftar Proyek Redmine</h1>
-      {projects.map((project) => (
-        <div key={project.id}>
-          <h2>{project.name}</h2>
-          <p>{project.description}</p>
-          <p>{project.created_on}</p>
-        </div>
-      ))}
+      <div className="project-list">
+        <h1>Daftar Proyek Redmine</h1>
+        {projects.slice(0, 2).map((project) => (
+          <div key={project.id} className="card">
+            <div className="card-body">
+              <h2>{project.name}</h2>
+              <p>
+                Deskripsi Project :
+                {' '}
+                {project.description}
+              </p>
+              <p>
+                Dibuat tanggal :
+                {' '}
+                {moment(project.created_on).format('DD MMMM YYYY')}
+              </p>
+            </div>
+          </div>
+        ))}
+        {projects.length > 2 && (
+          <a href="/all-projects">Lihat Semua Proyek</a>
+        )}
+      </div>
     </div>
   );
 };
---
+++ src/store/useAuthStore.js
@@ -0,0 +1,8 @@
+import create from 'zustand';
+
+const useAuthStore = create((set) => ({
+  token: null,
+  setToken: (token) => set({ token }),
+}));
+
+export default useAuthStore;
